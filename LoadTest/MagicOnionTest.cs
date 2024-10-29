ede6a86a59a2e5a13cd555af43699db2a2d04092
toutou <toutounode@gmail.com>
2024-10-30 01:20:04 +0900

    仮実装

---
+++ LoadTest/MagicOnionTest.cs
@@ -0,0 +1,62 @@
+﻿using DFrame;
+using Grpc.Net.Client;
+using MagicOnion.Client;
+using Shared.Interfaces;
+namespace LoadTest;
+
+public class MagicOnionStreamingHubTest : Workload, IGamingHubReceiver
+{
+    DateTime beginTime;
+    DateTime endTime;
+    int executeCount;
+    GrpcChannel channel = default!;
+    IGamingHub client = default!;
+
+    public override async Task SetupAsync(WorkloadContext context)
+    {
+        beginTime = DateTime.Now;
+        channel = GrpcChannel.ForAddress("http://localhost:5059");
+        client = await StreamingHubClient.ConnectAsync<IGamingHub, IGamingHubReceiver>(channel, this);
+        await client.JoinAsync("room", "user", new UnityEngine.Vector3(), new UnityEngine.Quaternion());
+        endTime = DateTime.Now;
+        executeCount++;
+    }
+
+    public override async Task ExecuteAsync(WorkloadContext context)
+    {
+        await client.MoveAsync(new UnityEngine.Vector3(), new UnityEngine.Quaternion());
+    }
+
+    public override async Task TeardownAsync(WorkloadContext context)
+    {
+        await client.LeaveAsync();
+        await client.DisposeAsync();
+        await channel.ShutdownAsync();
+        channel.Dispose();
+    }
+
+    public void OnJoin(Player player)
+    {
+        Console.WriteLine("Join Player:" + player.Name);
+    }
+
+    public void OnLeave(Player player)
+    {
+        Console.WriteLine("Leave Player:" + player.Name);
+    }
+
+    public void OnMove(Player player)
+    {
+        //Console.WriteLine("Move Player:" + player.Name);
+    }
+    
+    public override Dictionary<string, string>? Complete(WorkloadContext context)
+    {
+        return new()
+        {
+            { "begin", beginTime.ToString() },
+            { "end", endTime.ToString() },
+            { "count", executeCount.ToString() },
+        };
+    }
+}
---
+++ LoadTest/Program.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable CS1998
+
+using DFrame;
+using Microsoft.Extensions.DependencyInjection;
+using System.Buffers;
+
+var builder = DFrameApp.CreateBuilder(7312, 7313);
+builder.ConfigureWorker(options =>
+{
+    options.VirtualProcess = 32;
+    options.Metadata = new()
+    {
+        { "MachineName", Environment.MachineName },
+        { "ProcessorCount", Environment.ProcessorCount.ToString() }
+    };
+});
+
+if (args.Length == 0)
+{
+    // local, run both(host WebUI on http://localhost:portWeb)
+    await builder.RunAsync();
+}
+else if (args[0] == "controller")
+{
+    // listen http://*:portWeb as WebUI and http://*:portListenWorker as Worker listen gRPC
+    await builder.RunControllerAsync();
+}
+else if (args[0] == "worker")
+{
+    // worker connect to (controller) address.
+    // You can also configure from appsettings.json via builder.ConfigureWorker((ctx, options) => { options.ControllerAddress = "" });
+    await builder.RunWorkerAsync("http://foobar:5556");
+}
